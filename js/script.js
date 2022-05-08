9cd34de33a94b2c7b3770c57427103bdd6bc00ea
RyanDelos <delossantosrryan@gmail.com>
2022-05-08 16:52:58 -0700

    adding sticky navigation
    sticky navigation implemented using JS

--- js/script.js
+++ js/script.js
@@ -1,5 +1,29 @@
 'use-strict';
 
+// STICKY NAVIGATION
+
+const sectionHeroEl = document.querySelector('.hero-section');
+
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+
+    if (ent.isIntersecting === false) {
+      document.body.classList.add('sticky');
+    }
+
+    if (ent.isIntersecting === true) {
+      document.body.classList.remove('sticky');
+    }
+  },
+  {
+    root: null,
+    threshold: 0,
+    rootMargin: '-80px',
+  }
+);
+obs.observe(sectionHeroEl);
+
 // SLIDER FUNCTION
 const slider = function () {
   const slides = document.querySelectorAll('.slide');
